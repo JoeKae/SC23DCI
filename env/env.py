8a8f8163f5509a34c8a2384903e5da557d9a47e1
JoeKae <52297861+JoeKae@users.noreply.github.com>
2024-02-01 21:55:29 +0100

    adds optional env keys with default values

--- env/env.py
+++ env/env.py
@@ -13,39 +13,50 @@ class Env:
     Provides the static getEnv method
     """
 
-    validKeys = [
+    requiredKeys = [
         'MQTT_BROKER_IP',
-        'MQTT_TOPIC_TEMPERATURE',
-        'MQTT_TOPIC_ALL',
-        'MQTT_TOPIC_POWERSTATE',
-        'MQTT_TOPIC_POWERSTATE_SET',
-        'MQTT_TOPIC_MODE_SET',
-        'MQTT_TOPIC_SETPOINT_SET',
-        'MQTT_TOPIC_FLAP_MODE',
-        'MQTT_TOPIC_FLAP_MODE_SET',
-        'MQTT_TOPIC_FAN_SPEED',
-        'MQTT_TOPIC_FAN_SPEED_SET',
-        'MQTT_TOPIC_LWT',
-        'SC23DCI_IP',
-        'SC23DCI_POLL_INTERVAL',
-        'SC23DCI_MAX_TEMP_C',
-        'SC23DCI_MIN_TEMP_C',
-        'MQTT_HASSIO_AUTODETECT',
-        'MQTT_HASSIO_OBJECT_ID'
+        'SC23DCI_IP'
     ]
+
+    optional_keys = {
+        'MQTT_BROKER_PORT': 1883,
+        'MQTT_TOPIC_TEMPERATURE': 'sc23dci/sensors/temperature/ac',
+        'MQTT_TOPIC_ALL': 'sc23dci/all',
+        'MQTT_TOPIC_POWERSTATE': 'sc23dci/powerstate',
+        'MQTT_TOPIC_POWERSTATE_SET': 'sc23dci/powerstate/set',
+        'MQTT_TOPIC_MODE_SET': 'sc23dci/mode/set',
+        'MQTT_TOPIC_SETPOINT_SET': 'sc23dci/setpoint/set',
+        'MQTT_TOPIC_FLAP_MODE': 'sc23dci/flap_mode',
+        'MQTT_TOPIC_FLAP_MODE_SET': 'sc23dci/flap_mode/set',
+        'MQTT_TOPIC_FAN_SPEED': 'sc23dci/fan_speed',
+        'MQTT_TOPIC_FAN_SPEED_SET': 'sc23dci/fan_speed/set',
+        'MQTT_TOPIC_LWT': 'sc23dci/lwt',
+        'MQTT_HASSIO_AUTODETECT': True,
+        'MQTT_HASSIO_OBJECT_ID': 'SC23DCI-unique-id-not-set',
+        'MQTT_HASSIO_TOPIC': 'homeassistant',
+        'SC23DCI_MAX_TEMP_C': 31,
+        'SC23DCI_MIN_TEMP_C': 16,
+        'SC23DCI_POLL_INTERVAL': 10
+    }
 
     @staticmethod
     def get_env(key: str) -> Optional[str]:
         """
         Grants read access to valid environment variable
         :param key: The name of the variable to be read
-        :raises Exception: 'Invalid env key requested'
+        :raises KeyError: 'Invalid env key requested'
         :return: The value of the Variable
         """
 
-        if key not in Env.validKeys:
-            raise Exception('Invalid env key requested')
-        return os.getenv(key)
+        if key not in Env.requiredKeys and key not in Env.optional_keys:
+            raise KeyError('Invalid env key requested')
+        env_value = os.getenv(key)
+        if env_value is not None:
+            return env_value
+        elif key in Env.optional_keys:
+            return Env.optional_keys[key]
+        else:
+            raise KeyError('Invalid env key requested')
 
     @staticmethod
     def check_missing():
@@ -55,7 +66,7 @@ class Env:
         :return: A list of missing variable names
         """
         missing_envs = []
-        for key in Env.validKeys:
+        for key in Env.requiredKeys:
             env_key = os.getenv(key)
             if env_key is None or env_key == '':
                 missing_envs.append(key)
